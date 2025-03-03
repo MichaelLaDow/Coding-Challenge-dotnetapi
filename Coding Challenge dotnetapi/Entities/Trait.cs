95a0b9563b58697ebb6dfc85bca29f93bc3a9402
Alex Koumoundouros <akka.agk@gmail.com>
2025-03-03 18:03:09 +1100

    Added InMemoryDatabase (#1)
    Co-authored-by: Alex.Koumoundouros <akoumoundouros@gmail.com>

--- Coding Challenge dotnetapi/Entities/Trait.cs
+++ Coding Challenge dotnetapi/Entities/Trait.cs
@@ -1,7 +1,10 @@
-﻿namespace Coding_Challenge_dotnetapi.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Coding_Challenge_dotnetapi.Entities
 {
     public class Trait
     {
+        [Key]
         public int TraitId { get; set; }
         public string Name { get; set; }
         public int ModifierValue { get; set; }
